2b9be5a980a0c4f22f4ab54c16ebb0b7569bdd9a
NTIEmil <emil.winroth@elev.ga.ntig.se>
2024-01-25 11:01:11 +0100

    Nya meddelanden och sparad historik

--- chatApp/dist/script.js
+++ chatApp/dist/script.js
@@ -1,13 +1,18 @@
+const socket = io();
+
 $(() => {
   $("#send").click(() => {
     let message = { name: $("#name").val(), message: $("#message").val() };
     postMessage(message);
+    $("#message").val("");
   });
   getMessages();
 });
 
+socket.on("message", addMessage);
+
 function addMessage(message) {
-  $("#messages").append(`<h4>${message.name}</h4><p>${message.message}</p>`);
+  $("#messages").prepend(`<h4>${message.name}</h4><p>${message.message}</p>`);
 }
 
 function getMessages() {
--- chatApp/server.ts
+++ chatApp/server.ts
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 // 402 Payment Required
 
@@ -12,10 +14,35 @@ const app = express();
 app.use(express.static("./dist"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let messages = [
-  { name: "Mathias", message: "Hej" },
-  { name: "Henrik", message: "Hellå" },
-];
+const fs = require("fs");
+
+// @ts-ignore
+let messages;
+
+// @ts-ignore
+// let messages: [any];
+
+// @ts-ignore
+fs.readFile("./messages.json", "utf8", (error, json) => {
+  if (error) {
+    console.log(error);
+    app.res.sendStatus(500);
+    return;
+  }
+  try {
+    messages = JSON.parse(json);
+  } catch (error) {
+    console.log(error);
+    app.res.sendStatus(500);
+  }
+
+  app.res.sendStatus(200);
+});
+
+// let messages = [
+//   { name: "Mathias", message: "Hej" },
+//   { name: "Henrik", message: "Hellå" },
+// ];
 
 // @ts-ignore
 app.get("/messages", (req, res) => {
@@ -26,12 +53,33 @@ app.get("/messages", (req, res) => {
 app.post("/messages", (req, res) => {
   try {
     messages.push(req.body);
-    res.sendStatus(200);
+    io.emit("message", req.body);
+
+    // @ts-ignore
+    fs.writeFile(
+      "./messages.json",
+      JSON.stringify(messages),
+      "utf8",
+      (error) => {
+        if (error) {
+          console.log(error);
+          res.sendStatus(500);
+        } else {
+          console.log("writeFile complete");
+          res.sendStatus(200);
+        }
+      }
+    );
   } catch {
     res.sendStatus(400);
   }
 });
 
-app.listen(3000, () => {
+// @ts-ignore
+io.on("connection", (socket) => {
+  console.log("Användare ansluten");
+});
+
+http.listen(3000, () => {
   console.log("Servern körs, besök http://localhost:3000");
 });
