90bf21c6ed30d6761b3453258314e12f4b783e1c
Emil Winroth <119424476+Winroth13@users.noreply.github.com>
2024-03-14 10:01:01 +0100

    Kommentarer

--- inloggningssida/app.ts
+++ inloggningssida/app.ts
@@ -2,16 +2,17 @@ const express = require("express");
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 const path = require("path");
+// Importerar bcryptjs för att kryptera lösenord
 const bcrypt = require("bcryptjs");
 
 const app = express();
 app.set("view engine", "hbs");
 dotenv.config({ path: "./.env" });
 
+// Regex för att validera e-postadresser och lösenord
 const email_Regex = new RegExp(
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 );
-
 const password_Regex = new RegExp(
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
 );
@@ -26,10 +27,11 @@ const db = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
+// Funktion för att kolla om användaren redan finns i databasen
 const checkUserExists = async (column, value) => {
   return new Promise((resolve, reject) => {
     db.query(
-      `SELECT * FROM users WHERE ${column} = ?`,
+      `SELECT ${column} FROM users WHERE ${column} = ?`,
       [value],
       (error, result) => {
         if (error) {
@@ -69,33 +71,40 @@ app.get("/login", (req, res) => {
 });
 
 // Tar emot poster från registeringsformuläret
+// Kollar att informationen stämmer och skickar tillbaka ett meddelande med resultatet
+// Alla kollar som görs i webbläsaren gjörs även på servern ifall anvndaren skulle gå förbi webbläsarens kollar
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, password_confirm } = req.body;
-  let databaseDenial = false;
-
+  // Kollar om det redan finns en användare med samma namn
   if (await checkUserExists("name", name)) {
     return res.render("register", {
       message: "Användarnamnet är upptaget",
     });
+  // Kollar om det redan finns en användare med samma e-postadress
   } else if (await checkUserExists("email", email)) {
     return res.render("register", {
       message: "E-postadressen är upptagen",
     });
+  // Kollar om e-postadressen är i korrekt format
   } else if (email_Regex.test(email) == false) {
     return res.render("register", {
       message: "Ogiltig e-postadress",
     });
+  // Kollar om lösenordet är tillräckligt säkert
   } else if (password_Regex.test(password) == false) {
     return res.render("register", {
       message:
         "Lösenordet måste ha minst 8 tecken, en stor bokstav, en liten bokstav, ett nummer och ett specialtecken.",
     });
+  // Kollar om lösenorden matchar
   } else if (password !== password_confirm) {
     return res.render("register", {
       message: "Lösenorden matchar ej",
     });
   } else {
+    // Krypterar lösenordet
     bcrypt.hash(password, 10, function (err, hash) {
+      // Lägger till användaren i databasen
       db.query(
         "INSERT INTO users SET?",
         { name: name, email: email, password: hash },
@@ -114,6 +123,8 @@ app.post("/auth/register", async (req, res) => {
 });
 
 // Tar emot poster från loginsidan
+// Meddelandet som skciaks tillbaka om någonting är fel säger itne längre vad som är fel
+// Detta för att inte ge bort information om en sådan användare finns eller inte
 app.post("/auth/login", (req, res) => {
   const { name, password } = req.body;
 
