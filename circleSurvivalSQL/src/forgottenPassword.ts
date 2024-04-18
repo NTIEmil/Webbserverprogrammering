787aa8ceae589a7606a48c9ce0d8af2b3cdebeef
Emil Winroth <119424476+Winroth13@users.noreply.github.com>
2024-04-18 13:27:14 +0200

    Nollställ lösenordssida

---
+++ circleSurvivalSQL/src/forgottenPassword.ts
@@ -0,0 +1,30 @@
+/* När hemsidan har laddats in */
+window.onload = function () {
+  /* Hämtar elementen */
+  let email = document.getElementById("email-frg")! as HTMLInputElement;
+  let messageElement = document.querySelector<HTMLElement>(".alert-danger")!;
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const errorMessage = urlParams.get("message");
+
+  if (errorMessage) {
+    messageElement.textContent = errorMessage;
+  }
+
+  /* Döljer varnignsmeddelandet om det är tomt */
+  if (messageElement.textContent == "") {
+    messageElement.style.display = "none";
+  }
+
+  /* Om användaren försöker skicka formuläret utan att fylla i alla fält så skciaks det
+        inte iväg och hen får ett felmeddelande*/
+  document
+    .getElementById("submit-button")!
+    .addEventListener("click", function (event) {
+      if (email.value == "") {
+        event.preventDefault();
+        messageElement.textContent = "Fill in your email address";
+        messageElement.style.display = "block";
+      }
+    });
+};
